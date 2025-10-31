f5457bff0a7ef27d3b594c357258de95acc86549
jjrosen <joshrosen281@gmail.com>
2025-10-30 20:58:14 -0500

    made create method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,5 +8,16 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     render template: "products/index"
-  end 
+  end
+  
+  def create
+    @product = Poduct.create(
+      name: Faker::Appliance.equipment,
+    price: Faker::Commerce.price,
+    image_url: Faker::LoremFlickr.image(search_terms: ['appliance']),
+    description: Faker::Lorem.paragraph(sentence_count: 6),
+    seller: Faker::Company.name
+    )
+  end
+
 end
