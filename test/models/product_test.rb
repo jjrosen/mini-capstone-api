7dafbd31249a6c584f2038be043227de795f3d64
jjrosen <joshrosen281@gmail.com>
2025-11-06 16:46:46 -0600

    created tests for money sign, and create and update time; but i still have a problem with my total test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -12,5 +12,23 @@ class ProductTest < ActiveSupport::TestCase
     assert_equal "$0.9", product.tax
   end
 
+  # test "total" do 
+  #   product = Product.new(price: 10)
+  #   assert_equal "$10.90", product.total
+  # end
 
+  test "price_money_sign" do 
+    product = Product.new(price: 10)
+    assert_equal "$10", product.price_money_sign
+  end
+
+  test "easy_created_at" do 
+    product = Product.new(created_at: "2025-11-05T01:34;27.224Z")
+    assert_equal "11/05/2025 1:34 AM", product.easy_created_at
+  end
+
+  test "easy_updated_at" do 
+    product = Product.new(updated_at: "2025-11-05T01:34;27.224Z")
+    assert_equal "11/05/2025 1:34 AM", product.easy_updated_at
+  end
 end
